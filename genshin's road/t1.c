8d63758adda0e8a36a670689bfbde31e8b76655b
Muffinsdoo <pdudu2007@outlook.com>
2025-10-24 12:00:06 +0800

    t1v4.0

--- genshin's road/t1.c
+++ genshin's road/t1.c
@@ -1,49 +1,18 @@
 #include <stdio.h>
 
-int ten(int n);
-
 int main()
 {
-    long n;
-    int system;
-    scanf("%ld%d",&n,&system);
-    if(n == 1) printf("0001");
-    else if(n==0) printf("0000");
-    else{
-        long t=1;
-        int i=0;
-        while(t<=n)
-        {
-            t*=system;
-            i++;
-        }
-        t/=system;
-        long sum = 0;
-    for (int k=i;k>=1;k--)
+    int a,b,c[4]={0};
+    scanf("%d%d",&a,&b);
+    for (int i=0;i<=3;i++)
     {
-       sum+=n/t * ten(k-1);
-       n%=t;
-       t/=system;
+        c[i] = a % b;
+        a /= b;
     }
-    if(i<4)
+    for(int i=3;i>=0;i--)
     {
-        for(int t=1;t<=4-i;t++)
-        {
-            printf("0");
-        }
+        printf("%d",c[i]);
     }
-    printf("%ld",sum);
-}
     return 0;
 }
 
-int ten(int n)
-{
-    int t=1;
-    for(int i=1;i<=n;i++)
-    {
-        t*=10;
-    }
-    if(n==0) return 1;
-    else return t;
-}
